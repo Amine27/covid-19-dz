0455cb571187bb050eefa6b47afb4dfdca94cad8
Amine Roukh <amineroukh@gmail.com>
2020-03-26 17:37:20 +0100

    [master]: Update stats

--- map.js
+++ map.js
@@ -24,7 +24,7 @@ info.onAdd = function (map) {
 };
 
 info.update = function (props) {
-  this._div.innerHTML = '<span class="confirmed">Confirmed: 302</span><br><span class="recovered">Recovered: 89</span><br><span class="deaths">Deaths: 21</span><br><br>'
+  this._div.innerHTML = '<span class="confirmed">Confirmed: 367</span><br><span class="recovered">Recovered: 29</span><br><span class="deaths">Deaths: 25</span><br><br>'
     + (props ? '<b>' + props.NAME_1 + '</b><br />' + props.CASES + (props.CASES > 1 ? ' cases</sup>' : ' case</sup>') : 'Hover over a state');
 };
 

2be3606cff34b579d3e26bc4af14fcf733bb9f0f
Amine Roukh <amineroukh@gmail.com>
2020-04-10 17:41:40 +0200

    [master]: Updates stats

--- data.js
+++ data.js
@@ -1,60 +1,60 @@
-const date = ['2/25/20', '2/26/20', '2/27/20', '2/28/20', '2/29/20', '3/1/20', '3/2/20', '3/3/20', '3/4/20', '3/5/20', '3/6/20', '3/7/20', '3/8/20', '3/9/20', '3/10/20', '3/11/20', '3/12/20', '3/13/20', '3/14/20', '3/15/20', '3/16/20', '3/17/20', '3/18/20', '3/19/20', '3/20/20', '3/21/20', '3/22/20', '3/23/20', '3/24/20', '3/25/20', '3/26/20', '3/27/20', '3/28/20', '3/29/20', '3/30/20', '3/31/20', '4/1/20', '4/2/20', '4/3/20', '4/4/20', '4/5/20', '4/6/20', '4/7/20', '4/8/20', '4/9/20']
-const confirmed = [1,1,1,1,1,1,3,5,12,12,17,17,19,20,20,20,24,26,37,48,54,60,74,90,102,139,201,230,264,302,367,409,454,511,584,716,847,986,1171,1251,1320,1423,1468,1572,1666]
-const recovered = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,8,10,12,12,12,12,12,12,22,23,23,24,24,29,29,31,31,37,46,61,61,62,90,90,90,113,237,347]
-const deaths = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,4,4,6,9,11,15,17,17,19,21,25,26,29,31,35,44,58,83,105,130,152,173,193,205,235]
+const date = ['2/25/20', '2/26/20', '2/27/20', '2/28/20', '2/29/20', '3/1/20', '3/2/20', '3/3/20', '3/4/20', '3/5/20', '3/6/20', '3/7/20', '3/8/20', '3/9/20', '3/10/20', '3/11/20', '3/12/20', '3/13/20', '3/14/20', '3/15/20', '3/16/20', '3/17/20', '3/18/20', '3/19/20', '3/20/20', '3/21/20', '3/22/20', '3/23/20', '3/24/20', '3/25/20', '3/26/20', '3/27/20', '3/28/20', '3/29/20', '3/30/20', '3/31/20', '4/1/20', '4/2/20', '4/3/20', '4/4/20', '4/5/20', '4/6/20', '4/7/20', '4/8/20', '4/9/20', '4/10/20']
+const confirmed = [1,1,1,1,1,1,3,5,12,12,17,17,19,20,20,20,24,26,37,48,54,60,74,90,102,139,201,230,264,302,367,409,454,511,584,716,847,986,1171,1251,1320,1423,1468,1572,1666,1761]
+const recovered = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,8,10,12,12,12,12,12,12,22,23,23,24,24,29,29,31,31,37,46,61,61,62,90,90,90,113,237,347,405]
+const deaths = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,4,4,6,9,11,15,17,17,19,21,25,26,29,31,35,44,58,83,105,130,152,173,193,205,235,256]
 const gender = ['Man', 'Woman']
-const genderData = [971, 695]
+const genderData = [1025, 736]
 const age = ['< 1', '1 - 14', '15 - 24', '25 - 49', '50 - 59', '+60', 'N/A']
-const ageConfirmedData = [1, 30, 48, 588, 309, 643, 47]
-const ageDeathsData = [0, 1, 0, 16, 52, 166, 0]
-const lastUpdated = '2020-04-09 17:40'
+const ageConfirmedData = [1, 31, 50, 617, 323, 688, 51]
+const ageDeathsData = [0, 1, 0, 19, 55, 181, 0]
+const lastUpdated = '2020-04-10 17:41'
 const provinces = {
-  "Aïn Defla": { confirmed: 33, recovered: 0, deaths: 5, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-27" },
-  "Aïn Témouchent": { confirmed: 10, recovered: 0, deaths: 2, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
+  "Aïn Defla": { confirmed: 40, recovered: 0, deaths: 5, new_confirmed: 7, new_recovered: 0, new_deaths: 0, reported: "2020-03-27" },
+  "Aïn Témouchent": { confirmed: 13, recovered: 0, deaths: 2, new_confirmed: 3, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
   "Adrar": { confirmed: 4, recovered: 0, deaths: 1, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-15" },
-  "Alger": { confirmed: 291, recovered: 11, deaths: 62, new_confirmed: 14, new_recovered: 0, new_deaths: 13, reported: "2020-03-13" },
-  "Annaba": { confirmed: 14, recovered: 3, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-16" },
-  "Béchar": { confirmed: 10, recovered: 0, deaths: 0, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-04-03" },
-  "Béjaïa": { confirmed: 74, recovered: 1, deaths: 8, new_confirmed: 5, new_recovered: 0, new_deaths: 2, reported: "2020-03-17" },
-  "Batna": { confirmed: 18, recovered: 0, deaths: 4, new_confirmed: 2, new_recovered: 0, new_deaths: 1, reported: "2020-03-26" },
-  "Biskra": { confirmed: 10, recovered: 2, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-25" },
-  "Blida": { confirmed: 508, recovered: 32, deaths: 63, new_confirmed: 14, new_recovered: 0, new_deaths: 5, reported: "2020-03-01" },
-  "Bordj Bou Arréridj": { confirmed: 32, recovered: 0, deaths: 8, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-16" },
-  "Bouira": { confirmed: 12, recovered: 0, deaths: 0, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-16" },
-  "Boumerdès": { confirmed: 19, recovered: 1, deaths: 4, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-18" },
-  "Chlef": { confirmed: 27, recovered: 0, deaths: 1, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
-  "Constantine": { confirmed: 31, recovered: 0, deaths: 5, new_confirmed: 4, new_recovered: 0, new_deaths: 0, reported: "2020-03-22" },
-  "Djelfa": { confirmed: 21, recovered: 0, deaths: 1, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-27" },
+  "Alger": { confirmed: 310, recovered: 11, deaths: 68, new_confirmed: 19, new_recovered: 0, new_deaths: 6, reported: "2020-03-13" },
+  "Annaba": { confirmed: 14, recovered: 3, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-16" },
+  "Béchar": { confirmed: 10, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-04-03" },
+  "Béjaïa": { confirmed: 77, recovered: 1, deaths: 8, new_confirmed: 3, new_recovered: 0, new_deaths: 0, reported: "2020-03-17" },
+  "Batna": { confirmed: 18, recovered: 0, deaths: 4, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-26" },
+  "Biskra": { confirmed: 10, recovered: 2, deaths: 3, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-25" },
+  "Blida": { confirmed: 529, recovered: 32, deaths: 69, new_confirmed: 21, new_recovered: 0, new_deaths: 6, reported: "2020-03-01" },
+  "Bordj Bou Arréridj": { confirmed: 37, recovered: 0, deaths: 8, new_confirmed: 5, new_recovered: 0, new_deaths: 0, reported: "2020-03-16" },
+  "Bouira": { confirmed: 13, recovered: 0, deaths: 1, new_confirmed: 1, new_recovered: 0, new_deaths: 1, reported: "2020-03-16" },
+  "Boumerdès": { confirmed: 25, recovered: 1, deaths: 5, new_confirmed: 6, new_recovered: 0, new_deaths: 1, reported: "2020-03-18" },
+  "Chlef": { confirmed: 30, recovered: 0, deaths: 1, new_confirmed: 3, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
+  "Constantine": { confirmed: 33, recovered: 0, deaths: 5, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-03-22" },
+  "Djelfa": { confirmed: 21, recovered: 0, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-27" },
   "El Bayadh": { confirmed: 1, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-04-06" },
-  "El Oued": { confirmed: 16, recovered: 0, deaths: 4, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-18" },
+  "El Oued": { confirmed: 16, recovered: 0, deaths: 4, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-18" },
   "El Tarf": { confirmed: 4, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-27" },
   "Ghardaïa": { confirmed: 2, recovered: 0, deaths: 1, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-27" },
   "Guelma": { confirmed: 6, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-14" },
   "Illizi": { confirmed: 2, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-31" },
-  "Jijel": { confirmed: 18, recovered: 0, deaths: 3, new_confirmed: 1, new_recovered: 0, new_deaths: 2, reported: "2020-03-23" },
-  "Khenchela": { confirmed: 6, recovered: 0, deaths: 1, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-03-20" },
-  "Laghouat": { confirmed: 3, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-26" },
-  "M'Sila": { confirmed: 8, recovered: 0, deaths: 3, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-31" },
+  "Jijel": { confirmed: 21, recovered: 0, deaths: 3, new_confirmed: 3, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
+  "Khenchela": { confirmed: 6, recovered: 0, deaths: 1, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-20" },
+  "Laghouat": { confirmed: 8, recovered: 0, deaths: 0, new_confirmed: 5, new_recovered: 0, new_deaths: 0, reported: "2020-03-26" },
+  "M'Sila": { confirmed: 8, recovered: 0, deaths: 4, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-31" },
   "Médéa": { confirmed: 26, recovered: 1, deaths: 8, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-18" },
-  "Mascara": { confirmed: 20, recovered: 2, deaths: 2, new_confirmed: 3, new_recovered: 0, new_deaths: 2, reported: "2020-03-05" },
-  "Mila": { confirmed: 3, recovered: 0, deaths: 0, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-04-04" },
-  "Mostaganem": { confirmed: 27, recovered: 0, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
+  "Mascara": { confirmed: 20, recovered: 2, deaths: 3, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-05" },
+  "Mila": { confirmed: 3, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-04-04" },
+  "Mostaganem": { confirmed: 29, recovered: 0, deaths: 2, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
   "Naâma": { confirmed: 1, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-04-06" },
-  "Oran": { confirmed: 101, recovered: 0, deaths: 8, new_confirmed: 7, new_recovered: 0, new_deaths: 2, reported: "2020-03-19" },
+  "Oran": { confirmed: 104, recovered: 0, deaths: 9, new_confirmed: 3, new_recovered: 0, new_deaths: 1, reported: "2020-03-19" },
   "Ouargla": { confirmed: 4, recovered: 1, deaths: 1, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-02-25" },
-  "Oum el Bouaghi": { confirmed: 15, recovered: 0, deaths: 3, new_confirmed: 4, new_recovered: 0, new_deaths: 0, reported: "2020-03-26" },
+  "Oum el Bouaghi": { confirmed: 17, recovered: 0, deaths: 3, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-03-26" },
   "Relizane": { confirmed: 9, recovered: 0, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-21" },
-  "Sétif": { confirmed: 76, recovered: 2, deaths: 8, new_confirmed: 10, new_recovered: 0, new_deaths: 0, reported: "2020-03-19" },
-  "Saïda": { confirmed: 0, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "" },
-  "Sidi Bel Abbès": { confirmed: 16, recovered: 0, deaths: 0, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
-  "Skikda": { confirmed: 21, recovered: 3, deaths: 0, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-12" },
+  "Sétif": { confirmed: 80, recovered: 2, deaths: 8, new_confirmed: 4, new_recovered: 0, new_deaths: 0, reported: "2020-03-19" },
+  "Saïda": { confirmed: 1, recovered: 0, deaths: 0, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-04-10" },
+  "Sidi Bel Abbès": { confirmed: 17, recovered: 0, deaths: 0, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
+  "Skikda": { confirmed: 21, recovered: 3, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-12" },
   "Souk Ahras": { confirmed: 6, recovered: 0, deaths: 1, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-12" },
   "Tébessa": { confirmed: 6, recovered: 0, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-04-03" },
   "Tamanghasset": { confirmed: 0, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "" },
-  "Tiaret": { confirmed: 4, recovered: 0, deaths: 1, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-04-02" },
+  "Tiaret": { confirmed: 4, recovered: 0, deaths: 1, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-04-02" },
   "Tindouf": { confirmed: 0, recovered: 0, deaths: 0, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "" },
-  "Tipaza": { confirmed: 52, recovered: 0, deaths: 6, new_confirmed: 9, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" },
-  "Tissemsilt": { confirmed: 14, recovered: 1, deaths: 2, new_confirmed: 2, new_recovered: 0, new_deaths: 0, reported: "2020-03-19" },
-  "Tizi Ouzou": { confirmed: 47, recovered: 2, deaths: 7, new_confirmed: 4, new_recovered: 0, new_deaths: 0, reported: "2020-03-12" },
+  "Tipaza": { confirmed: 55, recovered: 0, deaths: 7, new_confirmed: 3, new_recovered: 0, new_deaths: 1, reported: "2020-03-23" },
+  "Tissemsilt": { confirmed: 15, recovered: 1, deaths: 2, new_confirmed: 1, new_recovered: 0, new_deaths: 0, reported: "2020-03-19" },
+  "Tizi Ouzou": { confirmed: 47, recovered: 2, deaths: 8, new_confirmed: 0, new_recovered: 0, new_deaths: 1, reported: "2020-03-12" },
   "Tlemcen": { confirmed: 38, recovered: 0, deaths: 2, new_confirmed: 0, new_recovered: 0, new_deaths: 0, reported: "2020-03-23" }
 }

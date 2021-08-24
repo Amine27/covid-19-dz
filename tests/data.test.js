5394c737c0eaa1a3c2473b22ef866ce9931ce4e8
Amine Roukh <amineroukh@gmail.com>
2021-08-24 21:39:03 +0200

    [master]: Test describe abbv

--- tests/data.test.js
+++ tests/data.test.js
@@ -10,56 +10,56 @@ describe('Data test routines', () => {
   const PROVINCES_NUMBER = 48
   const POPULATION_TOTAL = 43900000
 
-  test('test that date, confirmed, recovered and deaths arrays have the same length', () => {
+  test('date, confirmed, recovered and deaths arrays have the same length', () => {
     expect(date.length).toBe(confirmed.length)
     expect(date.length).toBe(recovered.length)
     expect(date.length).toBe(deaths.length)
   })
 
-  test('test that last date format is correct', () => {
+  test('last date format is correct', () => {
     expect(moment(date[date.length - LAST_VALUE], 'M/D/YY', true).isValid()).toBeTruthy()
   })
 
-  test('test that last date value is correct', () => {
+  test('last date value is correct', () => {
     const newDay = date[date.length - LAST_VALUE]
     const calcNewDay = moment(date[date.length - BEFORE_LAST_VALUE], 'M/D/YY').add(1, 'day').format('M/D/YY')
     expect(newDay).toBe(calcNewDay)
   })
 
-  test('test that last confirmed value is correct', () => {
+  test('last confirmed value is correct', () => {
     expect(confirmed[confirmed.length - LAST_VALUE]).toBeGreaterThanOrEqual(confirmed[confirmed.length - BEFORE_LAST_VALUE])
   })
 
-  test('test that last recovered value is correct', () => {
+  test('last recovered value is correct', () => {
     expect(recovered[recovered.length - LAST_VALUE]).toBeGreaterThanOrEqual(recovered[recovered.length - BEFORE_LAST_VALUE])
   })
 
-  test('test that last deaths value is correct', () => {
+  test('last deaths value is correct', () => {
     expect(deaths[deaths.length - LAST_VALUE]).toBeGreaterThanOrEqual(deaths[deaths.length - BEFORE_LAST_VALUE])
   })
 
-  test('test that critical value is correct', () => {
+  test('critical value is correct', () => {
     expect(Number.isNaN(critical)).toBeFalsy()
   })
 
-  test('test that lastUpdated format is correct', () => {
+  test('lastUpdated format is correct', () => {
     expect(moment(lastUpdated, 'YYYY-MM-DD HH:mm', true).isValid()).toBeTruthy()
   })
 
-  test('test that vaccinated date, fully and partly have the same length', () => {
+  test('vaccinated date, fully and partly have the same length', () => {
     expect(vaccinatedDate.length).toBe(vaccinatedFully.length)
     expect(vaccinatedDate.length).toBe(vaccinatedPartly.length)
   })
 
-  test('test that vaccinated values are correct', () => {
+  test('vaccinated values are correct', () => {
     expect(vaccinatedFully[vaccinatedFully.length - LAST_VALUE]).toBeLessThanOrEqual(vaccinatedPartly[vaccinatedPartly.length - LAST_VALUE])
   })
 
-  test('test that population is correct', () => {
+  test('population is correct', () => {
     expect(populationTotal).toBe(POPULATION_TOTAL)
   })
 
-  test('test that provinces length is correct', () => {
+  test('provinces length is correct', () => {
     expect(Object.keys(provinces).length).toBe(PROVINCES_NUMBER)
   })
 })
